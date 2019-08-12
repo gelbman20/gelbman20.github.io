c99ad0de0686bef09bb90574a980fc4fd966457a
gelbman20 <gelbman20@gmail.com>
2019-08-12 18:26:46 +0300

    FIX: an error when the input is empty

--- js/todos.js
+++ js/todos.js
@@ -1,50 +1,60 @@
-var input = document.querySelector('#input');
-var button = document.querySelector('#btn');
+const input = document.querySelector('#input');
+const button = document.querySelector('#btn');
 
-var todos = document.querySelector('#todos');
-var list = document.querySelector('#list');
-var todosArr = [
-  { id: 0, desc: 'drink water' },
-  { id: 1, desc: 'call 4 taxi' }
+const todos = document.querySelector('#todos');
+const list = document.querySelector('#list');
+
+let todosArr = [{
+    id: 0,
+    desc: 'drink water'
+  },
+  {
+    id: 1,
+    desc: 'call 4 taxi'
+  }
 ];
 
-function createItem(arr, desc) {
-  var newObject = {};
-  newObject.id = arr[arr.length - 1].id + 1;
-  newObject.desc = desc;
-  return newObject;
-}
+// Functions
+let createItem = (arr, desc) => {
+  let emptyObject = {};
+  emptyObject.desc = desc;
 
-function clearInput(input) {
-  input.value = '';
-}
+  if (arr.length === 0) {
+    emptyObject.id = 0;
+    return emptyObject;
+  }
 
-function getId(item) {
-  var id = item.getAttribute('id');
-  return id;
-}
+  emptyObject.id = arr[arr.length - 1].id + 1;
+  return emptyObject;
+};
 
-function refreshList(arr) {
-  var counter = 0;
-  var newArr = arr.map(function (item) {
-    var renderObject = {};
+let clearInput = (input) => input.value = '';
+
+let getId = (item) => item.getAttribute('id');
+
+
+let refreshList = (arr) => {
+  let counter = 0;
+  let newArr = arr.map(function (item) {
+    let renderObject = {};
     renderObject.id = counter++;
     renderObject.desc = item.desc;
     return renderObject;
   });
   arr = newArr.map(item => item);
   return arr;
-}
+};
 
-function displayList(arr, list) {
-  list.innerHTML = arr.map(item => '<li>' + item.desc + ' <a href="" ' + 'id=' + item.id + ' >X</a>' + '</li>').join('')
-}
+let displayList = (arr, list) => {
+  list.innerHTML = arr
+                    .map(item => `<li>${item.desc} <a href="" id="${item.id}">X</a></li>`)
+                    .join('')
+};
 
-// Test
-function displayTest() {
-  var test = document.querySelector('#test');
-  test.innerHTML = JSON.stringify(todosArr, null, 2);;
-}
+let displayTest = () => {
+  let test = document.querySelector('#test');
+  test.innerHTML = JSON.stringify(todosArr, null, 2);
+};
 
 // Start
 displayList(todosArr, list);
@@ -58,8 +68,7 @@ todos.addEventListener('click', function (event) {
   if (event.target === button && input.value !== '') {
     input.classList.remove('error');
     var inputValue = input.value;
-
-    todosArr.push( createItem(todosArr, inputValue) );
+    todosArr.push(createItem(todosArr, inputValue));
     clearInput(input);
 
     displayList(todosArr, list);
@@ -68,9 +77,9 @@ todos.addEventListener('click', function (event) {
 
   if (event.target.innerHTML === 'X') {
     event.preventDefault();
-    var current = event.target;
+    let current = event.target;
 
-    todosArr.splice( getId(current), 1 );
+    todosArr.splice(getId(current), 1);
     todosArr = refreshList(todosArr);
 
     displayList(todosArr, list);
@@ -79,7 +88,7 @@ todos.addEventListener('click', function (event) {
 });
 
 
-input.addEventListener("keyup", function(event) {
+input.addEventListener("keyup", function (event) {
   event.preventDefault();
   if (event.keyCode === 13) {
     button.click();
